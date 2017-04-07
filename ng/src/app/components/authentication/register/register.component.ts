f57654f58b32b77e9430f66c7d72a6c856d8eadd
Joshuavm <joshuavanmeer@gmail.com>
2017-04-07 14:25:43 +0200

    added registration form

--- ng/src/app/components/authentication/register/register.component.ts
+++ ng/src/app/components/authentication/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+    private registerForm: FormGroup;
 
-  ngOnInit() {
-  }
+
+    registerNewUser (): void {
+        if (this.registerForm.valid) {
+            const newUser = {
+                name: this.registerForm.value.name,
+                username: this.registerForm.value.username,
+                email: this.registerForm.value.email,
+                password: this.registerForm.value.password
+            };
+            // this.authService.register(newUser).subscribe((res) => {
+            //     console.log(res);
+            // });
+        }
+    }
+
+
+    constructor (
+        private formBuilder: FormBuilder
+    ) {}
+
+
+    ngOnInit () {
+
+        this.registerForm = this.formBuilder.group({
+            name: ['', Validators.required],
+            username: ['', Validators.required],
+            email: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+
+    }
 
 }
